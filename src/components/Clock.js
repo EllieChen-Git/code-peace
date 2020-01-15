e0f72792fd0f776806a0769634ace150ede91499
Ricky <ironest@gmail.com>
2020-01-15 12:35:08 +1100

    Added hard-coded props

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class Clock extends Component {
+
+  recordings=
+  [
+    {
+      start_time: "9:00",
+      stop_time: "11:00"
+    }
+  ]
+
   render() {
     return "Clock";
   }
