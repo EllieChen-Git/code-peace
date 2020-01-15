ab5303ef6cc4c5d72bb026cac9e8feeb9c398b73
Ricky <ironest@gmail.com>
2020-01-15 15:38:27 +1100

    Added Clock code

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,16 +2,53 @@ import React, { Component } from "react";
 
 class Clock extends Component {
 
-  recordings=
-  [
-    {
-      start_time: "9:00",
-      stop_time: "11:00"
+  state = {
+    secondsPassed: 0
+  }
+
+  componentDidMount() {
+
+    const {isWorking, recordings } = this.props;
+
+    if(isWorking()) {
+      
+      const starTime = recordings[recordings.length - 1].start_time;
+      const currTime = Math.round(new Date().getTime() / 1000);
+
+      this.setState({secondsPassed: currTime - starTime})
     }
-  ]
+
+    setInterval( () => {
+      let { secondsPassed } = this.state;
+      secondsPassed++;
+      this.setState( { secondsPassed } )
+    },
+    1000 );
+
+  }
+
+  getTimerFormat = () => {
+
+    const { secondsPassed } = this.state;
+
+    const seconds = secondsPassed % 60;
+    const minutes = Math.floor( secondsPassed / 60 );
+    const hours = Math.floor( secondsPassed / 60 / 60 );
+    
+
+    return `${hours}:${minutes}:${seconds}`
+
+  }
 
   render() {
-    return "Clock";
+
+    const { isWorking } = this.props;
+
+    return(
+            <>
+            { isWorking() && this.getTimerFormat() }
+            </>
+    );
   }
 }
 
