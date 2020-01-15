fcb634a4fb41cc810e013b84d3acb995df4779a2
Ricky <ironest@gmail.com>
2020-01-15 14:18:42 +1100

    Added function logics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 import Output from "./components/Output";
 import Clock from "./components/Clock";
 import Button from "./components/Button";
 
-function App() {
-  return (
-    <>
-      <h1>Code Peace</h1>
-      <Output />
-      <Clock />
-      <Button />
-    </>
-  );
+class App extends Component {
+
+  state = {
+    recordings: [],
+    isWorking: false
+  }
+
+  componentDidMount(){
+
+    console.log("App component DID MOUNT");
+
+    let rawRecordings = localStorage.getItem("RECORDINGS");
+
+    console.log(`Storage has: ${rawRecordings}`);
+    
+    if (!rawRecordings){
+      rawRecordings = [];
+    } else {
+      rawRecordings = JSON.parse(rawRecordings);
+    }
+    this.setState({ recordings: rawRecordings })
+
+  }
+
+  isWorking(){
+
+    const lastRecording = this.recordings[ this.recordings.length - 1 ];
+    
+    if (lastRecording.stop_time){
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+  onClickButton(){
+    
+    const recordings = this.recordings;
+
+    const currTime = new Date();
+    const recording = Math.round(currTime.getTime() / 1000);
+
+    let lastRecording;
+    let newRecordingsState;
+
+    if (this.isWorking){
+
+      lastRecording = recordings[ recordings.length - 1 ]
+      lastRecording.stop_time = recording;
+
+      newRecordingsState = recordings.slice ( 0, recordings.length - 1 )
+      newRecordingsState.push( lastRecording )
+
+    } else {
+      lastRecording = { start_time: recording }
+      newRecordingsState = [ ...recordings ]
+      newRecordingsState.push( lastRecording )
+    }
+
+    localStorage.setItem("RECORDINGS", newRecordingsState);
+    this.setState({recordings: newRecordingsState});
+  }
+
+  render() {
+
+      const { recordings } = this.state;
+
+      return (
+        <>
+          <h1>Code Peace</h1>
+          <h2>{JSON.stringify(this.state.recordings)}</h2>
+          <Output isWorking={ this.isWorking } />
+          <Clock recordings={ recordings } />
+          <Button isWorking={ this.isWorking } onClickButton={ this.onClickButton } />
+        </>
+      );
+
+    }
 }
 
 export default App;
+
+
+const now = new Date()  
+const secondsSinceEpoch = Math.round(now.getTime() / 1000)  
